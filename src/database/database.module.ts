5f3205aaf54911e18c8f9aca0f07b4f6e9ce158b
fabioNog <fabio.nogueira.carmo@gmail.com>
2026-09-08 20:55:28 -0300

    Aceitando ssl pra prod

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,20 +8,22 @@ import { Contact } from '../modules/contact/entities/contact.entity';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
-        const isProduction = configService.get('NODE_ENV') === 'production';
-
-        return {
+        const config = {
           type: 'postgres' as const,
           host: configService.get('DB_HOST', 'localhost'),
-          port: configService.get<number>('DB_PORT', 5432),
+          port: configService.get('DB_PORT', 5432),
           username: configService.get('DB_USERNAME', 'professor'),
           password: String(configService.get('DB_PASSWORD', 'professor123')),
           database: configService.get('DB_DATABASE', 'professor_db'),
           entities: [Contact],
-          synchronize: true,
-          logging: !isProduction,
-          ssl: isProduction ? { rejectUnauthorized: false } : false,
-        };
+          synchronize: true, // 👈 DEVE SER TRUE
+          logging: true, // 👈 Ative para ver o SQL
+          ssl: true,
+          extra: {
+            auth: 'password',
+          },
+        }; // 👈 Debug
+        return config;
       },
       inject: [ConfigService],
     }),
